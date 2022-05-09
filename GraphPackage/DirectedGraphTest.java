e8a2c290ea5e91e443bd37808ba35af4b4478c21
luisaruiz0 <98635984+luisaruiz0@users.noreply.github.com>
2022-05-09 15:05:39 -0700

    unit testing added

---
+++ GraphPackage/DirectedGraphTest.java
@@ -0,0 +1,77 @@
+package GraphPackage;
+import ADTPackage.*;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class DirectedGraphTest {
+    @Test
+    void testGetBreadthFirstTraversal() 
+    {
+        DirectedGraph<String> BreadthTra = new DirectedGraph<>();
+        BreadthTra.addVertex("A");
+		BreadthTra.addVertex("B");
+		BreadthTra.addVertex("C");
+		BreadthTra.addVertex("D");
+		BreadthTra.addVertex("E");
+		BreadthTra.addVertex("F");
+		BreadthTra.addVertex("G");
+		BreadthTra.addVertex("H");
+		BreadthTra.addVertex("I");
+		
+		//Creating Edges E
+		BreadthTra.addEdge("A", "B");
+		BreadthTra.addEdge("A", "D");
+		BreadthTra.addEdge("A", "E");
+		BreadthTra.addEdge("B", "E");
+		BreadthTra.addEdge("D", "G");
+		BreadthTra.addEdge("E", "F");
+		BreadthTra.addEdge("E", "H");
+		BreadthTra.addEdge("G", "H");
+		BreadthTra.addEdge("F", "C");
+		BreadthTra.addEdge("F", "H");
+		BreadthTra.addEdge("H", "I");
+		BreadthTra.addEdge("C", "B");
+		BreadthTra.addEdge("I", "F");
+        BreadthTra.getBreadthFirstTraversal("A");
+        QueueInterface<String> bQueue =  BreadthTra.getBreadthFirstTraversal("A");
+        assertEquals("ABDEGFHCI", bQueue.returnQueue());
+    }
+
+    @Test
+    void testGetDepthFirstTraversal() 
+    {
+        DirectedGraph<String> DepthTra = new DirectedGraph<>();
+		
+		//Adding Vertices V
+		DepthTra.addVertex("A");
+		DepthTra.addVertex("B");
+		DepthTra.addVertex("C");
+		DepthTra.addVertex("D");
+		DepthTra.addVertex("E");
+		DepthTra.addVertex("F");
+		DepthTra.addVertex("G");
+		DepthTra.addVertex("H");
+		DepthTra.addVertex("I");
+		//Adding Edges E
+		DepthTra.addEdge("A", "B");
+		DepthTra.addEdge("A", "D");
+		DepthTra.addEdge("A", "E");
+		DepthTra.addEdge("B", "E");
+		DepthTra.addEdge("D", "G");
+		DepthTra.addEdge("E", "F");
+		DepthTra.addEdge("E", "H");
+		DepthTra.addEdge("G", "H");
+		DepthTra.addEdge("F", "C");
+		DepthTra.addEdge("F", "H");
+		DepthTra.addEdge("H", "I");
+		DepthTra.addEdge("C", "B");
+		DepthTra.addEdge("I", "F");
+		
+		DepthTra.getDepthFirstTraversal("A");
+        QueueInterface<String> dQueue =  DepthTra.getDepthFirstTraversal("A");
+        assertEquals("ABEFCHIDG", dQueue.returnQueue());
+    }
+   
+}
